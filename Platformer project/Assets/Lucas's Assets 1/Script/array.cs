13a3fe3f732cdf90b6d1d4bb6257c57fb745a391
John Kaser <20.john.kaser@ksd.org>
2018-12-06 11:53:26 -0800

    just a small update

---
+++ Platformer project/Assets/Lucas's Assets 1/Script/array.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class array : MonoBehaviour {
+    string[] names;
+	// Use this for initialization
+	void Start () {
+        names = new string[4];
+        names[0] = "Ben";
+        names[1] = "Wayne";
+        names[2] = "Waxillium";
+        names[3] = "Vin";
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
