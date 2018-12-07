2e6c938975c707796f2a22b36ff86479a0830a1c
Jason Sivak <20.jason.sivak@ksd.org>
2018-12-07 12:42:57 -0800

    Animation Scripts

---
+++ Platformer project/Assets/Jason's Assests/Scripts/LegsAni.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegsAni : MonoBehaviour {
+
+    public float X;
+    public float Y;
+
+    GameObject Player;
+
+    public float PlayerVelocityX;
+    public float PlayerVelocityY;
+
+    public int Direct;
+
+    int RunModify;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        X = Input.GetAxisRaw("Horizontal");
+        Y = Input.GetAxisRaw("Vertical");
+
+        PlayerVelocityX = Player.GetComponent<Rigidbody2D>().velocity.x;
+        PlayerVelocityY = Player.GetComponent<Rigidbody2D>().velocity.y;
+
+        Direct = Player.GetComponent<TorsoAni>().Direct;
+
+        if (Direct == -1)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            RunModify = -1;
+
+        }else
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            RunModify = 1;
+        }
+
+        if(PlayerVelocityX >= 0.5)
+        {
+            GetComponent<Animator>().SetInteger("Move", 1 * RunModify);
+        }else if(PlayerVelocityX > -0.5 && PlayerVelocityX < 0.5)
+        {
+            if(Direct == 0)
+            {
+                GetComponent<Animator>().SetBool("Middle", true);
+                GetComponent<Animator>().SetInteger("Move", 0);
+            }
+            else
+            {
+                GetComponent<Animator>().SetBool("Middle", false);
+                GetComponent<Animator>().SetInteger("Move", 0);
+            }
+        }
+        else
+        {
+            GetComponent<Animator>().SetInteger("Move", -1 * RunModify);
+        } 
+        
+
+
+
+    }
+}
---
+++ Platformer project/Assets/Jason's Assests/Scripts/TorsoAni.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorsoAni : MonoBehaviour {
+
+    public int Frame;
+    public bool Shoot;
+
+    public int Direct;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetComponent<Animator>().SetInteger("Frame", Frame);
+        GetComponent<Animator>().SetBool("Shoot", Shoot);
+
+        if(Frame == 1 || Frame == 5)
+        {
+            Direct = 0;
+        }else if (Frame <= 5 && Frame >= 2)
+        {
+            Direct = 1;
+        }else if (Frame > 6)
+        {
+            Direct = -1;
+        }
+
+
+	}
+}
