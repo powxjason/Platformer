d2df9688c9543369c8379c091897a35270b989cd
Jason Sivak <20.jason.sivak@ksd.org>
2018-12-07 13:41:47 -0800

    Start of animation controlls

---
+++ Platformer project/Assets/Jason's Assests/Scripts/LegsCont.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegsCont : MonoBehaviour {
+
+    public bool InUse = true;
+    public float X;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (InUse)
+        {
+
+            if (Input.GetButton("a"))
+            {
+                X = -1;
+            }
+            else if (Input.GetButton("d"))
+            {
+                X = 1;
+            }
+            else
+            {
+                X = 0;
+            }
+        }
+
+        GetComponent<LegsAni>().X = X;
+	}
+}
---
+++ Platformer project/Assets/Jason's Assests/Scripts/TorsoCont.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorsoCont : MonoBehaviour {
+
+    int x;
+    int y;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("up"))
+        {
+            y = 1;
+        }else if (Input.GetKey("down"))
+        {
+            y = -1;
+        }
+        else
+        {
+            y = 0;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            x = -1;
+        }
+        else if (Input.GetKey("right"))
+        {
+            x = 1;
+        }
+        else
+        {
+            x = 0;
+        }
+
+        Direction(x, y);
+
+
+
+    }
+
+    public void Direction(int x, int y)
+    {
+        if (x == 1)
+        {
+            if(y == 1)
+            {
+                Frame(2);
+            }else if (y == 0)
+            {
+                Frame(3);
+            }
+            else
+            {
+                Frame(4);
+            }
+
+        }else if (x == -1){
+
+            if (y == 1)
+            {
+                Frame(8);
+            }
+            else if (y == 0)
+            {
+                Frame(7);
+            }
+            else
+            {
+                Frame(6);
+            }
+
+        }
+        else
+        {
+            if (y == 1)
+            {
+                Frame(1);
+            }
+            else if (y == 0)
+            {
+               
+            }
+            else
+            {
+                Frame(5);
+            }
+
+        }
+
+
+    }
+
+    public void Frame(int F)
+    {
+
+    }
+
+
+}
