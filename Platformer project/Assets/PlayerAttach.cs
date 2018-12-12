453b89117b96b67a852fedb9a4fe743727c28a39
Noah Miller <19.noah.miller@ksd.org>
2018-12-12 13:44:07 -0800

    script

---
+++ Platformer project/Assets/PlayerAttach.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttach : MonoBehaviour {
+
+	public GameObject Player;
+
+     void OnTriggerEnter2D(Collider2D collision)
+     {
+        if(collision.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+     }
+     void OnTriggerExit2D(Collider2D collision)
+     {
+        if(collision.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+     }
+}
