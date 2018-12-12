8288bcd52bb2eabf0dc7bcbc59b0cc57adf4f760
Jason Sivak <20.jason.sivak@ksd.org>
2018-12-12 13:46:53 -0800

    Boss health bar

---
+++ Platformer project/Assets/Jason's Assests/Scripts/BrainHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainHealth : MonoBehaviour {
+
+    int health = 15;
+
+    public GameObject healthBar;
+
+	void Update () {
+        healthBar.GetComponent<Transform>().localScale = new Vector3(health, 1, 1);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
---
+++ Platformer project/Assets/Jason's Assests/Scripts/Teleporter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public Vector3 CameraTele;
+    public GameObject Camera;
+    public GameObject PlayerTeleMark;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.GetComponent<Transform>().position = PlayerTeleMark.GetComponent<Transform>().position;
+            Camera.GetComponent<Transform>().position = CameraTele;
+        }
+
+
+    }
+}
