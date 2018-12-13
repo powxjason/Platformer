853ea1188b9ca2b56d106cdf0de9d599653a5d18
Jason Sivak <20.jason.sivak@ksd.org>
2018-12-13 12:23:17 -0800

    Win Scene

--- Platformer project/Assets/Jason's Assests/Scripts/BrainHealth.cs
+++ Platformer project/Assets/Jason's Assests/Scripts/BrainHealth.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BrainHealth : MonoBehaviour {
 
+    public string WinScene;
     int health = 15;
 
     public GameObject healthBar;
 
 	void Update () {
-        if (health >= 0)
+        if (health > 0)
         {
             healthBar.GetComponent<Transform>().localScale = new Vector3(health, 0.5f , 1);
+        }else if (health == 0)
+        {
+            SceneManager.LoadScene(WinScene);
         }
 	}
 
